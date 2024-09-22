1e85bde491ce5eefb051128dabed6ba7965a5c6b
artyom-lobachev <artyom-lobachev@mail.ru>
2024-09-22 18:12:48 +0300

    two integers

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int a, b;
+    printf("Введите первое целое число: ");
+    scanf("%d", &a);
+    printf("Введите второе целое число: ");
+    scanf("%d", &b);
+    printf("Числа: %d, %d\n", a, b);
     return 0;
 }
