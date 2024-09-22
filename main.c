6d739009470cb62c9fc4d62ba4d64cd8d36398a5
artyom-lobachev <artyom-lobachev@mail.ru>
2024-09-22 18:19:14 +0300

    update main.c

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    printf("Введите первое целое число: ");
-    scanf("%d", &a);
-    printf("Введите второе целое число: ");
-    scanf("%d", &b);
-    printf("Числа: %d, %d\n", a, b);
-	printf("Сумма: %d\n", a + b);
+	int a, b;
+	printf("Введите первое целое число: ");
+	scanf("%d", &a);
+	printf("Введите второе целое число: ");
+	scanf("%d", &b);
+	printf("Числа: %d, %d\n", a, b);
+	printf("Разность: %d\n", a - b);
     return 0;
 }
